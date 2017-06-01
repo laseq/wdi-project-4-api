492e01fbced5289b4ca61ef81923fd92b29b87b4
laseq <lourenco.sequeira@outlook.com>
2017-06-01 14:46:37 +0100

    Set up basic user authentication with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'authentications/register'
+
+  get 'authentications/login'
+
   scope :api do
     resources :users
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
