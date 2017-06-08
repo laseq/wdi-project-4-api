ff4d8a263416ff3c1c809178eafc42a00d4e9615
laseq <lourenco.sequeira@outlook.com>
2017-06-08 22:30:04 +0100

    Wrote a custom method in the Group serializer to display the members' attendance statuses for each event_by_date. The normal serializer wasn't displaying it as it was probably nested too deep

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,32 +10,14 @@ class Group < ApplicationRecord
 
   def events_by_date
     events.group_by{ |event| event.start_time.strftime("%a %d %b %Y") }
-    # t.strftime("%d:%m:%Y")
   end
 
-  # def members
-  #   accepted = []
-  #   accepted = Group.find(self.id).invitedmembers.map { |user| user.requests_as_receiver.where("group_id = self.id}", group_id).first.status == 'accepted'}
-  #   accepted
-  # end
-
-
-  # def accepted_members
-  #   invited_members.map { |member| member.status == 'accepted' }
-  # end
   def accepted_members
     invited_members.select { |member| member.requests_as_receiver.where(["group_id = ?", self.id]).first.status == 'accepted' }
   end
 
-  # def rejected_members
-  #   members.map { |member| member.status == 'rejected' }
-  # end
-  #
   def pending_members
     invited_members.select { |member| member.requests_as_receiver.where(["group_id = ?", self.id]).first.status == 'pending' }
   end
-  # def pending_members
-  #   invited_members.map { |member| member.status == 'pending' }
-  # end
 
 end
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -7,6 +7,39 @@ class GroupSerializer < ActiveModel::Serializer
   has_many :events
   has_many :events_by_date
 
-
   # has_many :members
+
+  def events
+    customised_events = []
+
+    object.events.each do |event|
+      custom_event = event.attributes
+      custom_event[:members_attending] = event.members_attending.collect{|member| member.slice(:id, :first_name, :last_name, :location, :email, :image, :phone)}
+      custom_event[:members_not_attending] = event.members_not_attending.collect{|member| member.slice(:id, :first_name, :last_name, :location, :email, :image, :phone)}
+      custom_event[:members_pending] = event.members_pending.collect{|member| member.slice(:id, :first_name, :last_name, :location, :email, :image, :phone)}
+      customised_events.push(custom_event)
+    end
+
+    return customised_events
+  end
+
+  def events_by_date
+  customised_events_by_date = []
+    object.events_by_date.each do |date, events|
+      custom_events_by_date = {}
+      customised_events = []
+      events.each do |event|
+        e1 = Event.find(event[:id])
+        custom_event = e1.attributes
+        custom_event[:members_attending] = e1.members_attending.select(User.column_names - ["created_at", "updated_at", "password_digest"])
+        custom_event[:members_not_attending] = e1.members_not_attending.select(User.column_names - ["created_at", "updated_at", "password_digest"])
+        custom_event[:members_pending] = e1.members_pending.select(User.column_names - ["created_at", "updated_at", "password_digest"])
+        customised_events.push(custom_event)
+      end
+      custom_events_by_date[date] = customised_events
+      customised_events_by_date.push(custom_events_by_date)
+    end
+    return customised_events_by_date
+  end
+
 end
