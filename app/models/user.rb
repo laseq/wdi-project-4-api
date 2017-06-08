44df9684dd5b51633c0244e35cefc9690a26803d
laseq <lourenco.sequeira@outlook.com>
2017-06-08 10:31:56 +0100

    Wrote code to get all the events for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,23 @@ class User < ApplicationRecord
   has_many :requests_as_sender, -> { distinct }, foreign_key: "sender_id", class_name: "Request"
   has_many :requests_as_receiver, -> { distinct }, foreign_key: "receiver_id", class_name: "Request"
   has_many :groups_as_member, -> { distinct }, through: :requests_as_receiver, source: :group
+  has_many :all_groups
+  has_many :upcoming_events
   validates :username, presence: true, uniqueness: true
+
+
+  def all_groups
+    (self.groups_as_creator + self.groups_as_member).uniq
+  end
+
+  def upcoming_events
+    # events = u1.all_groups.map(&:events)
+    # concatenated = events.flatten
+    # concatenated.sort_by{|event| event.start_time}
+    self.all_groups.map(&:events).flatten.sort_by{|event| event.start_time}
+  end
+
+
+
+
 end
