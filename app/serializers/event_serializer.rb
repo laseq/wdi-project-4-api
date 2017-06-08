c53982df9006da9d0b670daf5622da196310d04e
laseq <lourenco.sequeira@outlook.com>
2017-06-08 13:16:37 +0100

    Added group to the event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_time, :end_time, :location, :description, :dress_code, :status
+  attributes :id, :name, :start_time, :end_time, :location, :description, :dress_code, :status, :group
   has_one :group
 
 end
