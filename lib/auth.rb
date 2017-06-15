6773012601c206536859c993ce98cc4a232a010d
laseq <lourenco.sequeira@outlook.com>
2017-06-15 11:59:56 +0100

    Copied lib file back into the root folder

---
+++ lib/auth.rb
@@ -0,0 +1,20 @@
+require 'jwt'
+
+class Auth
+
+  ALGORITHM = 'HS256'
+
+  def self.issue(payload, expiry=24.hours.from_now)
+    payload[:exp] = expiry.to_i
+    JWT.encode(payload, auth_secret, ALGORITHM)
+  end
+
+  def self.decode(token, leeway=0)
+    decoded = JWT.decode(token, auth_secret, true, { leeway: leeway, algorithm: ALGORITHM })
+    HashWithIndifferentAccess.new(decoded[0])
+  end
+
+  def self.auth_secret
+    ENV["AUTH_SECRET"]
+  end
+end
